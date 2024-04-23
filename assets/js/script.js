043a4599cc047b07af9ee184049cc9849d5460b2
liscannor <youthbrd@gmail.com>
2024-04-23 10:08:24 +0100

    Add questions and answers to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,48 @@ const startQuizBtn = document.getElementById('start-quiz-btn');
 
 let username = null;
 
+const questions = [
+    {
+        image: 'assets/images/corgi.webp',
+        correctAnswer: 'Corgi'
+    },
+    {
+        image: 'assets/images/pit-bull.webp',
+        correctAnswer: 'Pit Bull'
+    },
+    {
+        image: 'assets/images/beagle.webp',
+        correctAnswer: 'Beagle'
+    },
+    {
+        image: 'assets/images/boxer.webp',
+        correctAnswer: 'Boxer'
+    },
+    {
+        image: 'assets/images/bichon-frise.webp',
+        correctAnswer: 'Bichon-frise'
+    },
+    {
+        image: 'assets/images/retriever.webp',
+        correctAnswer: 'Retriever'  
+    },
+    {
+        image: 'assets/images/husky.webp',
+        correctAnswer: 'Husky'  
+    },
+    {
+        image: 'assets/images/spitz.webp',
+        correctAnswer: 'Spitz'  
+    },
+    {
+        image: 'assets/images/german-shepherd-dog.webp',
+        correctAnswer: 'Germ.-shepherd'  
+    },
+    {
+        image: 'assets/images/chow-chow.webp',
+        correctAnswer: 'Chow-chow'  
+    }
+];
 
 
 function startQuiz() {
